9a5d50fd628e985e997c6ca06a1cebe149e537b7
AshanSandeep06 <ashansandeep06@gmail.com>
2022-06-28 22:31:38 +0530

    content updated

--- src/lk/ijse/hibernate/controller/LoginFormController.java
+++ src/lk/ijse/hibernate/controller/LoginFormController.java
@@ -1,8 +1,11 @@
 package lk.ijse.hibernate.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -15,4 +18,20 @@ public class LoginFormController {
     public JFXButton btnLogin;
     public AnchorPane context;
     public ImageView imgPassword;
+
+    public void keyReleasedOnAction(KeyEvent keyEvent) {
+
+    }
+
+    public void loginOnAction(ActionEvent event) {
+
+    }
+
+    public void showPasswordOnAction(MouseEvent mouseEvent) {
+
+    }
+
+    public void hidePasswordOnAction(MouseEvent mouseEvent) {
+
+    }
 }
