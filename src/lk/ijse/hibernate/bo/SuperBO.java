6084c1e891770594e9fece40cb2479c44d82ed9f
AshanSandeep06 <ashansandeep06@gmail.com>
2022-06-28 13:18:31 +0530

    Refactored BO layer

---
+++ src/lk/ijse/hibernate/bo/SuperBO.java
@@ -0,0 +1,8 @@
+package lk.ijse.hibernate.bo;
+
+/**
+ * @author : Ashan Sandeep
+ * @since : 0.1.0
+ **/
+public interface SuperBO {
+}
---
+++ src/lk/ijse/hibernate/bo/custom/RoomBO.java
@@ -0,0 +1,8 @@
+package lk.ijse.hibernate.bo.custom;
+
+/**
+ * @author : Ashan Sandeep
+ * @since : 0.1.0
+ **/
+public interface RoomBO {
+}
