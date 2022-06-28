ad4009cb6b2e881082afeb47aea5cd2f5de31e1b
AshanSandeep06 <ashansandeep06@gmail.com>
2022-06-28 22:45:58 +0530

    Added ValidationUtil class for textfield validations

---
+++ src/lk/ijse/hibernate/util/ValidationUtil.java
@@ -0,0 +1,50 @@
+package lk.ijse.hibernate.util;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.TextField;
+
+import java.util.LinkedHashMap;
+import java.util.regex.Pattern;
+
+/**
+ * @author : Ashan Sandeep
+ * @since : 0.1.0
+ **/
+
+// This is for the validations
+public class ValidationUtil {
+    public static Object validate(LinkedHashMap<TextField, Pattern> map, JFXButton... btn){
+        for (TextField key : map.keySet()) {
+            Pattern pattern = map.get(key);
+            if (!pattern.matcher(key.getText()).matches()) {
+                addError(key, btn);
+                return key;
+            }
+            removeError(key, btn);
+        }
+        return true;
+    }
+
+    private static void addError(TextField textField, JFXButton... btn) {
+        if (textField.getText().length() > 0) {
+            /*textField.setStyle("-fx-border-width: 4");
+            textField.setStyle("-fx-border-color: red");*/
+            textField.setStyle("-fx-text-fill: RED");
+        }else{
+            textField.setStyle("-fx-text-fill: BLACK");
+        }
+        for (JFXButton button : btn) {
+            button.setDisable(true);
+        }
+    }
+
+    private static void removeError(TextField textField, JFXButton... btn) {
+        /*textField.setStyle("-fx-border-width: 4");
+        textField.setStyle("-fx-border-color: green");*/
+
+        textField.setStyle("-fx-text-fill: BLACK");
+        for (JFXButton button : btn) {
+            button.setDisable(false);
+        }
+    }
+}
